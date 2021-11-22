dfd847845192c5a0dacb6387b6b190c38edf6848
Prince of Zamunda <omarxsawyer@gmail.com>
2021-11-21 17:29:48 -0700

    styling

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+//Question Layouts//
+
+//What year were the Chicago Bulls founded?
+//1999
+//1987
+//1956
+//1966*
+
+
+//What was the first jersey number retired by the Bulls?
+//4*
+//23
+//33
+//1
+
+
+//Which Chicago Bulls coach has the highest winning percentage?
+//Tom Thibodeau
+//Doug Collins
+//Phil Jackson*
+//Jerry Sloan
+
+//How mancy sccoring titles did Michael Jordan win during his career?
+//8
+//10*
+//7
+//6
+
+//Who is the youngest player to win the NBA Most Valuable Player Award?
+//Scottie Pippen
+//Derrick Rose*
+//Jerry Sloan
+//Michael Jordan
