bc3275e56a2b4497c3cce5fdcf4ef457ce4f4334
Prince of Zamunda <omarxsawyer@gmail.com>
2021-11-21 23:11:10 -0700

    java script issue

--- Assets/script.js
+++ Assets/script.js
@@ -31,3 +31,27 @@
 //Derrick Rose*
 //Jerry Sloan
 //Michael Jordan
+
+
+// creat array and pass #, QA, and options
+const start_btn = document.querySelector(".start-btn button");
+const info_box = document.querySelector(".info-box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+//const quiz_box = info_box.querySelector(".quiz-box");
+
+
+// If start Quiz Button Clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");// show the info box
+}
+
+// If exit Button Clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); // hide the text
+}
+
+// If continue Button Clicked
+//continue_btn.onclick = ()=>{
+    //info_box.classList.remove("activeInfo");// hide the text
+   // info_box.classList.remove("activeInfo");// hide the text
